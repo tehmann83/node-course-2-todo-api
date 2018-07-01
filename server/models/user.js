4074b5909127908e7cdba6980af7bddac999e27d
tehmann83 <ehmannth@gmail.com>
2018-07-01 11:54:23 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -73,6 +73,22 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        return res? resolve(user) : reject();
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   let user = this;
 
--- server/server.js
+++ server/server.js
@@ -108,6 +108,24 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  // let email = req.body.email;
+  // let password = req.body.password;
+  let body = _.pick(req.body, ['email', 'password']);
+
+  // res.send({email, password});
+  //res.send(body);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    //res.send(user);
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
